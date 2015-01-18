49a91ffc3ad2298cfd4354023e4994e17c1b0ac5
Dareus Conover <admin-staff@sas.upenn.edu>
2015-01-18 17:50:36 -0500

    adding debug.output

--- bin/paper_db.py
+++ bin/paper_db.py
@@ -41,7 +41,7 @@ class PaperDB:
 
     def __setattr__(self, attr_name, attr_value):
         """debug.output() when a state variable is updated"""
-        class_name = self.__class__.__name__.lower
+        class_name = self.__class__.__name__.lower()
 
         ## we always use the lowercase of the class_name in the state variable
         #if attr_name == '{}_state'.format(class_name):
