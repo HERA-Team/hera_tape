a23b5975d2d4f6c9ba82975cfb375386dda783a4
Dareus Conover <admin-staff@sas.upenn.edu>
2016-11-14 12:44:33 -0500

    add setup stub and test script for enumerating data paths

---
+++ bin/paper_setup.py
@@ -0,0 +1,45 @@
+"""Handle configuration and setup of basic data paths
+
+   This module checks for basic file path configuration necessary to run backup
+to tape.
+
+"""
+
+import os
+import shutil
+import datetime
+
+from paper_debug import Debug
+
+def setup (init = False):
+    pass
+    
+    ## parse commandline argumetns
+
+    ## assume a config file exists, or fail
+    ## create a default config if requested (via init variable)
+    ## read the config file
+    ## check the configured paths for filesystem matchs
+    ## create any missing paths
+
+
+def config_check ():
+
+    ## config file
+    pass 
+
+
+class Install(object):
+
+    pass
+
+class InstallCheck(object):
+
+
+
+    ## read in the config file
+    
+    pass
+
+
+
